6941633dc43ec6fd44df0eef82624a98743f264a
imogenemslt <144333984+imogenemslt@users.noreply.github.com>
2024-09-03 16:48:02 +0000

    Update pg189_task_1-7.py

--- pg189_task_1-7.py
+++ pg189_task_1-7.py
@@ -1,30 +1,30 @@
-# #task 1
-# #age is the varible and when int n need for print
-# age = int(input("enter your age: "))
-# score = int(input("enter your test score: "))
-# if age >= 17:
-#     if score >= 80:
-#         #prints good if they are 17 or above and get abovw 80
-#         print ("good!!")
-#     else:
-#         #prints try harder if they are over 17 and get ess then 80
-#         print ("please try harder")
-# else:
-#     #prints excilent if they are under 17 and get above 80
-#     print("excilent")
+ #task 1
+ #age is the varible and when int n need for print
+ age = int(input("enter your age: "))
+ score = int(input("enter your test score: "))
+ if age >= 17:
+     if score >= 80:
+         #prints good if they are 17 or above and get abovw 80
+         print ("good!!")
+     else:
+         #prints try harder if they are over 17 and get ess then 80
+         print ("please try harder")
+ else:
+     #prints excilent if they are under 17 and get above 80
+     print("excilent")
 
-# #task 5
-# email= (input("enter your email it must contain a @ and a . and be 8 or more carictors to be valid:"))
-# target="@"
-# #see page 22
-# if (email.__contains__(target)) :
-#     if (email.__contains__(".")):
-#         if len(email) >= 8:
-#             print("valid email proceed")
-#         else:
-#             print("invalid try again")
-# else:
-#     print("invalid try again")
+ #task 5
+ email= (input("enter your email it must contain a @ and a . and be 8 or more carictors to be valid:"))
+ target="@"
+ #see page 22
+ if (email.__contains__(target)) :
+     if (email.__contains__(".")):
+         if len(email) >= 8:
+             print("valid email proceed")
+         else:
+             print("invalid try again")
+ else:
+     print("invalid try again")
 
 #task 7
 #makes the person add in lengh of sticks

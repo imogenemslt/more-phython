9a9405c11dc05c7f9b25a24b2f082740d16055af
imogenemslt <144333984+imogenemslt@users.noreply.github.com>
2024-09-30 18:06:12 +0100

    Update recurstion_1.py

--- recurstion_1.py
+++ recurstion_1.py
@@ -20,4 +20,16 @@ print(num, "=",function(num))
 #task 3 (but for loop)
 num = int(input("enter number: "))
 for i in range (num,1,-1):
+
+#task 1 pg 209
+def fibonacci():
+    fib = [0, 1]
+    while len(fib) < 8:
+        fib.append(fib[-1] + fib[-2])
+    return fib[:8]
+
+
+print(fibonacci())
+
+#task 2
     

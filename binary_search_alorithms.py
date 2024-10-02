8dd52abf08c8ddf1316dc69db11b8a564a11dc43
imogenemslt <144333984+imogenemslt@users.noreply.github.com>
2024-10-02 17:03:59 +0100

    Update binary_search_alorithms.py

---
+++ binary_search_alorithms.py
@@ -0,0 +1,17 @@
+list = [19,  87, 1, -1, 11, 0, 33, 19]
+#LIMEAR
+#set key to -1
+#select first item in  the list
+#chech if first item is = to the -1
+#if not move on to next number
+#if number mathces key stop and dispay message
+
+#BINEARY
+#set key as -1
+#sort list in order
+#set first and last item as 0 and -1
+#last- first floor divided by 2 to find midle value
+#check if midlle value is = -1
+#if it is end code
+#if not chech if middle number is greater or less then key
+#reapet 
